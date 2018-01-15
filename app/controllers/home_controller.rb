79721f9768aa2c0c435cba140dd03b7b6e3f0e29
Evan <srichsun@gmail.com>
2018-01-15 11:01:07 +0800

    利用controller傳給view的變數，控制partial要不要顯示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def boy
+    @show_love = true
+  end
+
+  def girl
+    @show_love = false
+  end
+
+  def friend
+    @show_love = true
+  end
 end
