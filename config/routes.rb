2760018619061b2a9114e867c0d136dec658968f
Evan <srichsun@gmail.com>
2018-01-15 10:12:59 +0800

    Add - 新增一些頁面

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  
+  get 'boy', to: "home#boy"
+  get 'girl', to: "home#girl"
+  get 'friend', to: "home#friend"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
