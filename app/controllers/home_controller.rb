e809c50e646a9fbe29af730f9d4c27f4c68aca11
Evan <srichsun@gmail.com>
2018-01-26 17:20:46 +0800

    建立種子資料，並修正分頁

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
 
-  PRODUCTS_COUNT = 80
   PRODUCTS_PER_PAGE_COUNT = 20
 
   def index
@@ -11,27 +10,10 @@ class HomeController < ApplicationController
     }
 
     @first_page_number = 1
-    @last_page_number = PRODUCTS_COUNT/PRODUCTS_PER_PAGE_COUNT
+    @last_page_number = Product.count / PRODUCTS_PER_PAGE_COUNT
 
     @products = Product.all
-    # @products = []
-    # (1..PRODUCTS_COUNT).each do |index|
-    #   product =
-    #   {
-    #     id: "#{index}",
-    #     name: "ocean",
-    #     description: "beautiful ocean",
-    #     image_url: "https://static.pexels.com/photos/128458/pexels-photo-128458.jpeg"
-    #   }
-    #
-    #   @products << product
-    # end
-
-    # params gets string so needs to_i.
-    # When at homeapge so params[:current_page] would be nil,
-    # then current_page_number would be -1, would show the last page,
-    # which is wrong, so need to set current_page is 1
     @current_page_number = params[:current_page] ? params[:current_page].to_i : 1
-    # @products = @products[(@current_page_number-1)*PRODUCTS_PER_PAGE_COUNT, PRODUCTS_PER_PAGE_COUNT]
+    @products = @products.offset((@current_page_number - 1) * PRODUCTS_PER_PAGE_COUNT).limit(PRODUCTS_PER_PAGE_COUNT)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+PRODUCTS_COUNT = 80
+
+PRODUCTS_COUNT.times do
+  product =
+  {
+    name: "ocean",
+    description: "beautiful ocean",
+    image_url: "https://static.pexels.com/photos/128458/pexels-photo-128458.jpeg"
+  }
+
+  Product.create(product)
+end
