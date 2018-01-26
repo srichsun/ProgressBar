0a57d701bab6b09c70cd1ed1909cef58cc39337c
Evan <srichsun@gmail.com>
2018-01-26 16:53:26 +0800

    用讀資料庫的方式取代原始讀檔案的方式

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,24 +13,25 @@ class HomeController < ApplicationController
     @first_page_number = 1
     @last_page_number = PRODUCTS_COUNT/PRODUCTS_PER_PAGE_COUNT
 
-    @products = []
-    (1..PRODUCTS_COUNT).each do |index|
-      product =
-      {
-        id: "#{index}",
-        name: "ocean",
-        description: "beautiful ocean",
-        image_url: "https://static.pexels.com/photos/128458/pexels-photo-128458.jpeg"
-      }
-
-      @products << product
-    end
+    @products = Product.all
+    # @products = []
+    # (1..PRODUCTS_COUNT).each do |index|
+    #   product =
+    #   {
+    #     id: "#{index}",
+    #     name: "ocean",
+    #     description: "beautiful ocean",
+    #     image_url: "https://static.pexels.com/photos/128458/pexels-photo-128458.jpeg"
+    #   }
+    #
+    #   @products << product
+    # end
 
     # params gets string so needs to_i.
     # When at homeapge so params[:current_page] would be nil,
     # then current_page_number would be -1, would show the last page,
     # which is wrong, so need to set current_page is 1
     @current_page_number = params[:current_page] ? params[:current_page].to_i : 1
-    @products = @products[(@current_page_number-1)*PRODUCTS_PER_PAGE_COUNT, PRODUCTS_PER_PAGE_COUNT]
+    # @products = @products[(@current_page_number-1)*PRODUCTS_PER_PAGE_COUNT, PRODUCTS_PER_PAGE_COUNT]
   end
 end
---
+++ db/migrate/20180125093333_add_columns_to_products.rb
@@ -0,0 +1,7 @@
+class AddColumnsToProducts < ActiveRecord::Migration[5.1]
+  def change
+    add_column :products, :description, :string
+    add_column :products, :image_url, :string
+    add_column :products, :price, :integer
+  end
+end
