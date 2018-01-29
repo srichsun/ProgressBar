797361e9a2807d95806b919acc93a159d374d82f
Evan <srichsun@gmail.com>
2018-01-29 13:25:46 +0800

    示範用primary key取代預設的id，但沒事不要用

--- db/migrate/20180129051344_create_categories.rb
+++ db/migrate/20180129051344_create_categories.rb
@@ -1,6 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[5.1]
   def change
-    create_table :categories do |t|
+    create_table :categories, id: false do |t|
+      t.primary_key :kkk
       t.string :name
       t.text :description
       t.string :image_url
