cc2c198ac1108b8c8994135ec390645f3a606a6e
Evan <srichsun@gmail.com>
2018-01-29 11:12:58 +0800

    用form_tag新增資料
    - 把舊的home controller東西移到products contrller
    - 用rest路由resources

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,3 @@
 class HomeController < ApplicationController
 
-  PRODUCTS_PER_PAGE_COUNT = 20
-
-  def index
-    @ad = {
-      title: "Advmeds Design",
-      description: "Best Clinic reservation system",
-      action_title: "more"
-    }
-
-    @first_page_number = 1
-    @last_page_number = Product.count / PRODUCTS_PER_PAGE_COUNT
-
-    @products = Product.all
-    @current_page_number = params[:current_page] ? params[:current_page].to_i : 1
-    @products = @products.offset((@current_page_number - 1) * PRODUCTS_PER_PAGE_COUNT).limit(PRODUCTS_PER_PAGE_COUNT)
-  end
 end
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,46 @@
+class ProductsController < ApplicationController
+
+  PRODUCTS_PER_PAGE_COUNT = 20
+
+  def index
+    @ad = {
+      title: "Advmeds Design",
+      description: "Best Clinic reservation system",
+      action_title: "more"
+    }
+
+    @first_page_number = 1
+    @last_page_number = Product.count / PRODUCTS_PER_PAGE_COUNT
+
+    @products = Product.all
+    @current_page_number = params[:current_page] ? params[:current_page].to_i : 1
+    @products = @products.offset((@current_page_number - 1) * PRODUCTS_PER_PAGE_COUNT).limit(PRODUCTS_PER_PAGE_COUNT)
+  end
+
+  def new
+    @note = flash[:note]
+  end
+
+  def create
+
+    # 把form送來的值存assign創造instance
+    # Product.create(
+    #   name:        params[:name],
+    #   description: params[:description],
+    #   image_url:   params[:image_url],
+    #   price:       params[:price]
+    # )
+    #
+    # 以上其實就是
+    # Product.create(params)
+    # 但是rails有防護機制，要特別允許的值才會讓你過，要像以下
+    product = Product.create(product_permit)
+
+    flash[:note] = "from create" #flash讓action之間傳遞值
+    redirect_to action: :new     #同一個controller下
+  end
+
+  def product_permit
+    params.permit([:name, :description, :image_url, :price])
+  end
+end
