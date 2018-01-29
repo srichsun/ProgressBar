17e83376de66c689ed1ea98b08b4243e70ee2e7a
Evan <srichsun@gmail.com>
2018-01-29 13:24:18 +0800

    新增category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125093333) do
+ActiveRecord::Schema.define(version: 20180129051344) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
