d8a784a17eae22efd7ef6407c85b197c53b9832c
Evan <srichsun@gmail.com>
2018-01-30 17:34:52 +0800

    Revert "使用者密碼存入資料庫時加密 - 簡易概念示範"
    This reverts commit 54493925cd3aa85de6641447669f4cd72920c03f.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,2 @@
 class User < ApplicationRecord
-  before_save :salted
-
-  def salted
-    # 在model裡面要找到創造object完的那個實例，用self
-    if self.id.blank?
-      self.password = 'aaaaa' + self.password
-    end
-  end
 end
