d1974a5bae5e5dc3c97bb600abebdf2de97ddee6
Evan <srichsun@gmail.com>
2018-01-30 16:33:29 +0800

    實作簡易登入登出 - flash就是一次性的session

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,20 @@
+class AdminController < ApplicationController
+
+  def log_in
+     @note = session[:note]
+     # @ note = flash[:note]
+  end
+
+  def log_out
+    session[:note] = nil
+  end
+
+  def create_session
+    user = User.find_by(email: params[:email], password: params[:password])
+    if user
+      session[:note] = user.name
+      # flash[:note] = user.name
+    end
+    redirect_to action: :log_in
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'products#index'
 
+  get 'admin/log_in', to: 'admin#log_in'
+  post 'admin/log_in', to: 'admin#create_session'
+  get 'admin/log_out', to: 'admin#log_out'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
