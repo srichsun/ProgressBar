6921cf947091a8eee0b6d68dfb38a2e576c99868
Evan <srichsun@gmail.com>
2018-01-31 12:01:07 +0800

    釐清實例變數生命週期
    - 在controller 拿到實例變數，class沒死，變數都還活著
    - 在view可以用helper拿到controller實例變數

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,19 @@ class ApplicationController < ActionController::Base
 
   def current_user
     # 如果已經有@current_user就不用一直select
-    if @current_user 
+    if @current_user
       return @current_user
     end
+    # class AdminController < ApplicationController
+    #   def action
+    #     current_user.name
+    #     current_user.name
+    #     # class AdminController 沒死
+    #     # 實例變數都會活著
+    #     # 所以第二次用current_user.name
+    #     # 就可以拿到 @current_user
+    #   end
+    # end
 
     current_user_id =  session[:current_user_id]
     if current_user_id
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,3 +3,6 @@ module ApplicationHelper
     return @current_user
   end
 end
+
+# 在view使用helper
+# 可以拿到controller得到的變數
