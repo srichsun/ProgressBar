1cc41e8d83fb695a43980ca2500d64290f4b3676
Evan <srichsun@gmail.com>
2018-01-31 16:53:57 +0800

    has many建立關聯

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  has_many :subcategories
+  # 這樣Category的物件就會有.subcategories等等方法可以用
+
+  # 如果寫
+  # has_many :children, class_name: 'Subcategory', foreign_key: 'category_id'
+  # 這樣當Category物件.children其實就是.subcategories
 end
