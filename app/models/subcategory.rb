aeec62f53e17c9d2187c1cc166d6ebeb39e87974
Evan <srichsun@gmail.com>
2018-02-01 15:01:26 +0800

    讓新增編輯產品時可以選類別 - 選擇框同時顯示主類別與父類別。

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -3,4 +3,9 @@ class Subcategory < ApplicationRecord
   # 以上就等於以下，後面可以省略不寫
   # belongs_to :category, class_name: 'Category', foreign_key: 'category_id'
   has_many :products
+
+  def name_with_category
+    "#{category.try(:name)}" + " / "+ "#{name}"
+    #等同於Subcategory物件.category, Subcategory物件.name
+  end
 end
