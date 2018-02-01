824227a75eb3b69629ae0bc1b457e0cff8be2221
Evan <srichsun@gmail.com>
2018-02-01 13:41:08 +0800

    顯示主類別與副類別 - 在seed.rb中將Products與Subcategories做關聯

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# user
+admin = User.create(
+  name: "rock",
+  email: "a@gmail.com",
+  password: "333",
+  is_admin: "true"
+)
+
+# category, subcategory
+categories = [
+  {
+    name: "3c",
+    subcategories: [
+      { name: "phone" },
+      { name: "computer" },
+      { name: "television" }
+    ]
+  },
+  {
+    name: "food",
+    subcategories: [
+      { name: "bread" },
+      { name: "all you can eat" },
+      { name: "hot pot"}
+    ]
+  }
+]
+
+categories.each do |c|
+  category = Category.create(name: c[:name])
+  c[:subcategories].each do |subc|
+      subcategory = category.subcategories.create(name: subc[:name])
+      # subcategory =Subcategory.create(name: subc[:name], category: category)
+      # 另外一種產生方法
+  end
+end
+
+# product
+subcategory = Subcategory.first
 PRODUCTS_COUNT = 80
 
 PRODUCTS_COUNT.times do
@@ -13,7 +52,8 @@ PRODUCTS_COUNT.times do
   {
     name: "ocean",
     description: "beautiful ocean",
-    image_url: "https://static.pexels.com/photos/128458/pexels-photo-128458.jpeg"
+    image_url: "https://static.pexels.com/photos/128458/pexels-photo-128458.jpeg",
+    subcategory: subcategory
   }
 
   Product.create(product)
