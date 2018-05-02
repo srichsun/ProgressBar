17dfef6e588f3d6a67dfedb2e1b4f59e3f4a9dda
Evan <srichsun@gmail.com>
2018-05-02 14:39:01 +0800

    Refactor - 用devise提供的route取代以前自己寫的登入登出route

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/srichsun_admin', as: 'rails_admin'
   root to: 'products#index'
 
-  get 'admin/log_in', to: 'admin#log_in'
-  post 'admin/log_in', to: 'admin#create_session'
-  get 'admin/log_out', to: 'admin#log_out'
-
-
   resources :categories, param: :category_id, only: [] do
     member do
       get :products
