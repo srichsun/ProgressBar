d1c997588b11d336776550129eda43c132bfaf08
Evan <srichsun@gmail.com>
2018-05-02 16:07:07 +0800

    Add - 新增model之間關聯

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :cart_items
+  has_many :order_items
   belongs_to :subcategory
   mount_uploader :image_url, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :carts
+  has_many :orders
+
+  after_create :create_carts
+
+  private
+  
+  def create_carts
+    if self.carts.blank?
+      Cart.buy_now.create(user: self)
+      Cart.buy_next_time.create(user: self)
+    end
+  end
 end
